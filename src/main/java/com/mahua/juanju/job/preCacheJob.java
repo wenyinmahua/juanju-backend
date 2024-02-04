b55f0a84c41b8fe0bbefb94524777000cb7bf113
ma <501847822@qq.com>
2024-02-04 15:41:41 +0800

    update 增加分布式锁

--- src/main/java/com/mahua/juanju/job/preCacheJob.java
+++ src/main/java/com/mahua/juanju/job/preCacheJob.java
@@ -6,6 +6,8 @@ import com.mahua.juanju.model.User;
 import com.mahua.juanju.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,34 +31,44 @@ public class preCacheJob {
 	@Resource
 	private RedisTemplate<String,Object> redisTemplate;
 
+	@Resource
+	private RedissonClient redissonClient;
+
 
 	private List<Long> mainUserList = Arrays.asList(1L);
 	//每天00 : 00点执行，预热推荐缓存
 	@Scheduled(cron = "0 0 0 * * ?")
 	public void doCacheRecommendUser(){
-		for (Long id : mainUserList) {
-			//预热推荐用户,只加载该用户的前64条数据
-			for(int pageNum = 1;pageNum <= 8; pageNum++){
-				String redisKey = String.format("juanju:user:recommend:%s:%s",id,pageNum);
-				ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
-				Page<User> userPage = (Page<User>) valueOperations.get(redisKey);
-
-				//无缓存，查数据库
-				QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-				Page<User> userPageList = userService.page(new Page<>(pageNum,8 ),queryWrapper);
-				List<User> userList = userPageList.getRecords();
-				userList = userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
-				userPageList.setRecords(userList);
-				int total = Math.min((int) userPageList.getTotal(), 64);
-				userPageList.setTotal(total);
-				try {
-					valueOperations.set(redisKey,userPageList,300000, TimeUnit.MILLISECONDS);
-				} catch (Exception e) {
-					log.error("redis set key error",e);
+		RLock lock = redissonClient.getLock("juanju:precachejob:docache:lock");
+		try {
+			if(lock.tryLock(0,-1,TimeUnit.MILLISECONDS)){
+				System.out.println("get lock"+Thread.currentThread().getId());
+				for (Long id : mainUserList) {
+					//预热推荐用户,只加载该用户的前64条数据，每页保存8条数据，
+					for(int pageNum = 1;pageNum <= 8; pageNum++){
+						String redisKey = String.format("juanju:user:recommend:%s:%s",id,pageNum);
+						ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
+						QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+						Page<User> userPageList = userService.page(new Page<>(pageNum,8 ),queryWrapper);
+						userPageList.setRecords(userPageList.getRecords().stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList()));
+						userPageList.setTotal(Math.min((int) userPageList.getTotal(), 64));
+						try {
+							valueOperations.set(redisKey,userPageList,300000, TimeUnit.MILLISECONDS);
+						} catch (Exception e) {
+							log.error("redis set key error",e);
+						}
+					}
 				}
 			}
-
+		} catch (InterruptedException e) {
+			log.error("doCacheRecommendUser err",e);
+		}finally {
+			if(lock.isHeldByCurrentThread()){
+				System.out.println("release lock：" + Thread.currentThread().getId());
+				lock.unlock();
+			}
 		}
+
 
 	}
 
